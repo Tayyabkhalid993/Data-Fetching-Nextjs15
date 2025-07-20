a8fc0b5d76420dbe7c6e1ced23286f557269f48c
Tayyab <tayyabkhalid993@gmail.com>
2025-07-20 23:10:29 +0500

    fetching data from database

--- src/app/prisma-db.ts
+++ src/app/prisma-db.ts
@@ -7,11 +7,13 @@ const addProduct = async () =>{
 
     if(productCount == 0){
         await prisma.product.createMany({
+            
             data:[
                 {title : "Product 1", price : 100, description: "This is product desc 1"},
                 {title : "Product 2", price : 200, description: "This is product desc 2"},
                 {title : "Product 3", price : 300, description: "This is product desc 3"},
             ]
+        
         })
     }
 }
